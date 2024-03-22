57c7dcb2a02c7cbbfd1420045cf9aefb0652da63
Rezaul Karim <rezaul.km105@gmail.com>
2024-03-22 08:46:07 +0000

    romved double class

--- src/components/GridHeader.js
+++ src/components/GridHeader.js
@@ -7,26 +7,26 @@ const GridHeader = (props) => {
         Total image(s) {props.totalImage}
       </span>
       <span className="text-gray-600 text-sm font-medium me-2 px-2.5 py-2">
-        Select grid column number :
+        Select album column size:
       </span>
       <button
         type="button"
         onClick={() => props.changeGridColumns(5)}
-        className="bg-slate-200 text-gray-600 text-sm font-medium   text-sm p-0.5 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-300"
+        className="bg-slate-200 text-gray-600 text-sm font-medium  p-0.5 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-300"
       >
         5
       </button>
       <button
         type="button"
         onClick={() => props.changeGridColumns(8)}
-        className="bg-slate-200 text-gray-600 text-sm font-medium  text-sm p-1 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-200"
+        className="bg-slate-200 text-gray-600 text-sm font-medium p-1 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-200"
       >
         8
       </button>
       <button
         type="button"
         onClick={() => props.changeGridColumns(10)}
-        className="bg-slate-200 text-gray-600 text-sm font-medium  text-sm p-1.5 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-200"
+        className="bg-slate-200 text-gray-600 text-sm font-medium  p-1.5 text-center inline-flex items-center me-2 dark:bg-blue-600 hover:bg-blue-200"
       >
         10
       </button>
