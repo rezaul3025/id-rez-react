c68d184794afabd4085ea0bc8dce70989131f69a
Rezaul Karim <rezaul.km105@gmail.com>
2024-03-22 11:14:50 +0100

    add search function favorites

--- src/pages/MyFavourites.js
+++ src/pages/MyFavourites.js
@@ -1,20 +1,32 @@
 import React from "react";
 import PhotoGrid from "../components/PhotoGrid";
+import PhotoSearch from "../components/PhotoSearch";
+
 import { useState, useEffect } from "react";
 
 const MyFavourites = () => {
     const [data, setData] = useState([]);
-
+    const [results, setResults] = useState([]);
+  
     useEffect(() => {
-        setData(JSON.parse(localStorage.getItem('saved_photos')));
+        let savedPhotos = JSON.parse(localStorage.getItem('saved_photos'));
+        setData(savedPhotos);
+        setResults(savedPhotos);
     }, []);
+
+    const doSearchOnCurrentResults = (event) => {
+        if (data != null) {
+            let searchItems = data.filter(function (entry) { return entry.name.includes(event.target.value); });
+            setResults(searchItems);
+        }
+    }
 
     return (
         <div className="relative mx-auto text-gray-600">
+            <PhotoSearch handleSearch={doSearchOnCurrentResults} />
             <div id="photos" className="pt-4">
-
                 {data != null &&
-                    <PhotoGrid results={data} />
+                    <PhotoGrid results={results} />
                 }
             </div>
         </div>
